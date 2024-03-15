58d5aa4685e1884e18e0f7dc00ae9f1f97ce5278
animward <animward@gmail.com>
2024-03-15 11:58:41 -0700

    refactor functions

--- index.js
+++ index.js
@@ -21,15 +21,13 @@ function newTask(title, description) {
 }
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
+function completeTask(task) {
+  task.complete = true;
 }
 
 // Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+function logTaskState(task) {
+  console.log(`${task.title} has${task.completecomplete ? " " : " not "}been completed`);
 }
 
 // DRIVER CODE BELOW
@@ -38,8 +36,8 @@ const task1 = newTask("Clean Cat Litter"); // task 0
 const task2 = newTask("Do Laundry"); // task 1
 const tasks = [task1, task2];
 
-console.log(tasks);
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+console.log(tasks);
